8c0157f361fbf69809ecafcf43a8a421ea8a73f3
lex <lex24@ukr.net>
2014-07-07 17:42:01 +0300

    changed main config

--- protected/config/main.php
+++ protected/config/main.php
@@ -49,19 +49,16 @@ return array(
 			),
 		),
 
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
+
 		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
+            'table_prefix' => 'tbl_',
 		),
-		*/
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
